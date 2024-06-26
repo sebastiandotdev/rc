4a3fbc0943591677e5d721cb37db38cfd816e3ff
Sebastián García <johancs.mm@gmail.com>
2024-06-26 09:16:05 -0500

    Update repository and package names (#4)

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import { Command } from 'commander'
 import packageJSON from '../package.json' assert { type: 'json' }
 
-const src = new Command()
+const rc = new Command()
 
-src
-  .name('src')
+rc
+  .name('rc')
   .usage('<command> [options]')
   .description('Source code management')
   .version(packageJSON.version, '-v, --version', 'output the current version')
   .helpOption('-h, --help', 'Display help for command')
   .allowUnknownOption()
 
-src.parse()
+rc.parse()
