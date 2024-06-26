2ca3bac1abd5a2e39b34b76d5e531e9725f264b0
castrogarciajs <johancs.mm@gmail.com>
2024-06-25 22:32:13 -0500

    Add bin/env.mjs for executing the CLI

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-export const build = 'build'
+import { Command } from 'commander'
+import packageJSON from '../package.json' assert { type: 'json' }
+
+const src = new Command()
+
+src
+  .name('src')
+  .usage('<command> [options]')
+  .description('Source code management')
+  .version(packageJSON.version, '-v, --version', 'output the current version')
+  .helpOption('-h, --help', 'Display help for command')
+  .allowUnknownOption()
+
+src.parse()
