f65e22221e6557b673c0fc9a682d222a4a534810
castrogarciajs <johancs.mm@gmail.com>
2024-06-28 17:45:38 -0500

    Update command lists

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,13 @@ import consola from 'consola'
 import chalk from 'chalk'
 import packageJSON from '../package.json' assert { type: 'json' }
 import { findMostMatchText } from './helpers/diff'
+import { methodDescriptions } from './constants/messages'
 
 const rc = new Command()
 
-export type CommandName = 'init'
+export type CommandName = 'init' | 'get' | 'post' | 'put' | 'patch' | 'delete'
 
-const commandList: CommandName[] = ['init']
+const commandList: CommandName[] = ['init', 'get', 'post', 'put', 'patch', 'delete']
 
 rc
   .name('rc')
@@ -47,11 +48,64 @@ rc
 
 rc
   .command('init')
-  .description(chalk.bold.gray('⚡ Welcome to the Simple REST Client CLI'))
-  .argument('[URL]', 'The URL of the API you want to interact with')
+  .description(chalk.bold.gray('Initialize a new REST Client'))
+  .argument('[BASE_URL]', 'The base URL of the API you want to interact with')
   .option('-m, --method <method>', 'HTTP method to use', 'GET')
   .action(() => {
     consola.info('init command')
+  })
+
+rc
+  .command('get')
+  .description(chalk.bold.gray('Make a GET request to the API'))
+  .argument(methodDescriptions)
+  .option('-i, --id <id>', 'The ID of the resource you want to get')
+  .option('-q, --query <query>', 'Query parameters to send with the request')
+  .option('-h, --header <header>', 'Headers to send with the request')
+  .action(() => {
+    consola.info('get command')
+  })
+
+rc
+  .command('post')
+  .description(chalk.bold.gray('Make a POST request to the API'))
+  .argument(methodDescriptions)
+  .option('-b, --body <body>', 'The body of the request')
+  .option('-h, --header <header>', 'Headers to send with the request')
+  .action(() => {
+    consola.info('post command')
+  })
+
+rc
+  .command('put')
+  .description(chalk.bold.gray('Make a PUT request to the API'))
+  .argument(methodDescriptions)
+  .option('-i, --id <id>', 'The ID of the resource you want to update')
+  .option('-b, --body <body>', 'The body of the request')
+  .option('-h, --header <header>', 'Headers to send with the request')
+  .action(() => {
+    consola.info('put command')
+  })
+
+rc
+  .command('patch')
+  .description(chalk.bold.gray('Make a PATCH request to the API'))
+  .argument(methodDescriptions)
+  .option('-i, --id <id>', 'The ID of the resource you want to update')
+  .option('-b, --body <body>', 'The body of the request')
+  .option('-h, --header <header>', 'Headers to send with the request')
+  .action(() => {
+    consola.info('patch command')
+  })
+
+rc
+  .command('delete')
+  .description(chalk.bold.gray('Make a DELETE request to the API'))
+  .argument(methodDescriptions)
+  .option('-i, --id <id>', 'The ID of the resource you want to delete')
+  .option('-h, --header <header>', 'Headers to send with the request')
+  .action(() => {
+    consola.info('delete command')
   })
 
 rc.parse()
