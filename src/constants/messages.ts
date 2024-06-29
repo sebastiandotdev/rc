d306ffbcde04ef0c280e04d3387d05af4a206f7b
castrogarciajs <johancs.mm@gmail.com>
2024-06-28 20:25:00 -0500

    Refactor utilities of messages and options

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -1,1 +1,5 @@
+export const descriptionRC = 'Simple REST Client is an easy-to-use command-line tool for interacting with RESTful APIs.'
 export const methodDescriptions = `[URL]', 'The URL of the API you want to interact with`
+
+export const helpDescription = 'Display help for command'
+export const headerDescription = 'Headers to send with the request'
