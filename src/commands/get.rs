1af8f3da2df917f675cd48c0e7e57b37bde450f5
Sebastián García <johancs.mm@gmail.com>
2024-08-12 20:25:43 -0500

    feat: read configuration file recursively (#38)

--- src/commands/get.rs
+++ src/commands/get.rs
@@ -1,11 +1,15 @@
 use anyhow::Error;
 
-use crate::args::flags;
+use crate::{args::flags, utils::read_config_file};
 
 pub struct Get;
 
 impl Get {
   pub fn new(get_flags: &flags::GetFlags) -> Result<(), Error> {
+    let config_rc = read_config_file()?;
+
+    println!("{:?}", config_rc);
+
     if let Some(id) = &get_flags.id {
       println!("O my is a id: {id}")
     }
